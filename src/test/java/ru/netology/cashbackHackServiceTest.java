7c2ad093500354edace029e00e6d309cd8770beb
Dima Zap <zapilenovd@gmail.com>
2022-02-16 20:35:18 +0300

    Test add

--- src/test/java/ru/netology/cashbackHackServiceTest.java
+++ src/test/java/ru/netology/cashbackHackServiceTest.java
@@ -1,4 +1,24 @@
+package ru.netology;
+
 import static org.testng.Assert.*;
+
 public class cashbackHackServiceTest {
-    
+
+    @org.testng.annotations.Test
+    public void shouldRemainIfAmountMoreBoundary() {
+        cashbackHackService cashbackHackService = new cashbackHackService();
+        int amount = 4500;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void shouldRemainIfAmountLessBoundary() {
+        cashbackHackService cashbackHackService = new cashbackHackService();
+        int amount = 950;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 50;
+        assertEquals(actual, expected);
+    }
+
 }
