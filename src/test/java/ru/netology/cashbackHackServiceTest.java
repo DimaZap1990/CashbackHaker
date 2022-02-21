255f0e8b06bdcc4bd124b731a97849186f1e38b8
Dima Zap <zapilenovd@gmail.com>
2022-02-21 20:57:47 +0300

    FaildTest add

--- src/test/java/ru/netology/cashbackHackServiceTest.java
+++ src/test/java/ru/netology/cashbackHackServiceTest.java
@@ -20,5 +20,13 @@ public class cashbackHackServiceTest {
         int expected = 50;
         assertEquals(actual, expected);
     }
+    @org.testng.annotations.Test
+    public void shouldRemainIfAmountEqualsBoundary() {
+        cashbackHackService cashbackHackService = new cashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
 
 }
